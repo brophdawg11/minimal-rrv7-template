e5e7c563ebb3c503dc0739397c080de9b5a97fd9
Matt Brophy <matt@brophy.org>
2025-01-03 13:48:32 -0500

    Updates

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,9 @@
 import type { Route } from "./+types/home";
 
-export default function Home({}: Route.ComponentProps) {
-  return <h1>Hello React Router</h1>;
+export function loader() {
+  return { name: "React Router" };
+}
+
+export default function Home({ loaderData }: Route.ComponentProps) {
+  return <h1>Hello {loaderData.name}</h1>;
 }
